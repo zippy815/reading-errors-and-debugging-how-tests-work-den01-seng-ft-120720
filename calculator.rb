0408858afd39038655b1fba6b40d03246ddbb11f
Zipporah Horch <zipporahhorch@gmail.com>
2020-10-21 01:32:45 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,6 @@
 first_number = 1
 second_number = 7
 sum = second_number + first_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
