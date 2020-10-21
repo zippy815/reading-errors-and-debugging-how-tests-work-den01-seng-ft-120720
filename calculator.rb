bbb77dd8bf8ecc0040adf4ef75fdfe37efbf865d
Zipporah Horch <zipporahhorch@gmail.com>
2020-10-21 01:32:58 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
 # Add your variables here
+first_number = 1
+second_number = 7
+sum = second_number + first_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
